8a78696416665fc64bf83e46555be12dbb7f90ac
RomainBoudet <romainoclock@gmail.com>
2021-11-20 15:18:04 +0100

    add swagger doc

--- app/router.js
+++ app/router.js
@@ -5,12 +5,18 @@ const router = Router();
 const mainController = require('./controllers/mainController');
 
 /**
- * 
+ * Génère un Cadex de manière aléatoire, sur un fond de nuage
+ * @route GET /cloud
+ * @group Cadex - génération de cadavre exquis
+ * @returns {string} 200 - le cadavre exquis généré sur un fond de nuage
  */
 router.get('/cloud', mainController.cloud);
 
 /**
- * 
+ * Génère un Cadex de manière aléatoire, sur un fond de d'étoiles
+ * @route GET /particles
+ * @group Cadex - génération de cadavre exquis
+ * @returns {string} 200 - le cadavre exquis généré sur un fond de d'étoiles
  */
 router.get('/particles', mainController.particles);
 
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const expressSwagger = require('express-swagger-generator')(app);
+let optionSwagger = require('./swagger-config.json');
+optionSwagger.basedir = __dirname;
+optionSwagger.swaggerDefinition.host = `localhost:${port}`;
+expressSwagger(optionSwagger);
+
 //app.use(helmet());
 
 app.use(express.static(`${__dirname}/app/static`));
@@ -15,9 +21,15 @@ app.use(express.static(`${__dirname}/app/static`));
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/app/views`);
 
-app.use(express.urlencoded({ extended: true }));
-
 app.use('/v1/cadex', router);
+
+/**
+ * Redirection vers une page 404
+ */
+ app.use((req, res) => {
+    res.status(404).redirect('/api-docs');
+  });
+
 
 app.listen(port, () => {
   console.log(`APIS Running on https://localhost:${port}/v1/cadex`);
