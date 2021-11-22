c7b83c33066f72dabef441c13729afec98c7b00b
RomainBoudet <romainoclock@gmail.com>
2021-11-22 13:53:50 +0100

    add header config

--- index.js
+++ index.js
@@ -42,9 +42,16 @@ app.use(helmet.contentSecurityPolicy({
   }))
 
 app.use((req, res, next) => {
+  res.setHeader(
+    "Permissions-Policy",
+    "geolocation=(), interest-cohort=(), fullscreen=(), accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), display-capture=(), document-domain=(), document-domain=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), gamepad=(), gyroscope=(), layout-animations=(), legacy-image-formats=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), oversized-images=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), speaker-selection=(), sync-xhr=(), usb=(), screen-wake-lock=(), web-share=(), xr-spatial-tracking=()"
+  );
     res.setHeader("X-XSS-Protection", "1; mode=block");
     next();
   });
+
+app.set('x-powered-by', false);
+
 
 app.use(express.static(`${__dirname}/app/static`));
 
